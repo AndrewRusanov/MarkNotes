ac6794d26eec7e1aa8ff75df68aa275cfa9d6968
Andrew Rusanov <rusanow.andrej2018@yandex.ru>
2025-06-25 07:49:07 +0300

    chore #24: delete console.log

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,7 +5,6 @@ import classNames from 'classnames'
 export function App() {
   const { theme } = useTheme()
 
-  console.log('####:', theme)
   return (
     <div className={classNames('app', theme)}>
       <h1>Привет, мир!</h1>
