3cc8a02860b6da5a1ca349d7508de0e4d5e6de7c
Andrew Rusanov <rusanow.andrej2018@yandex.ru>
2025-06-25 08:08:35 +0300

    feat #3: add browser router

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,16 @@
 import '@/app/styles/index.scss'
 import { useTheme } from '@app/providers'
 import classNames from 'classnames'
+import { Route, Routes } from 'react-router-dom'
 
 export function App() {
   const { theme } = useTheme()
 
   return (
     <div className={classNames('app', theme)}>
-      <h1>Привет, мир!</h1>
+      <Routes>
+        <Route path='/login' element={<div>LoginPage</div>} />
+      </Routes>
     </div>
   )
 }
