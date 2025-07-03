05c28ea217c62e4640a1cda6679f2bc4dba7e10d
Andrew Rusanov <rusanow.andrej2018@yandex.ru>
2025-07-03 17:19:48 +0300

    feat #28 : add logout

--- src/shared/icons/index.ts
+++ src/shared/icons/index.ts
@@ -1,6 +1,7 @@
 import { ErrorIcon } from './ErrorIcon'
 import { LoaderIcon } from './LoaderIcon'
+import { LogOutIcon } from './LogOutIcon'
 import { MoonIcon } from './MoonIcon'
 import { SunIcon } from './SunIcon'
 
-export { ErrorIcon, LoaderIcon, MoonIcon, SunIcon }
+export { ErrorIcon, LoaderIcon, LogOutIcon, MoonIcon, SunIcon }
--- src/widgets/Navbar/Navbar.tsx
+++ src/widgets/Navbar/Navbar.tsx
@@ -1,16 +1,33 @@
+import { useAuth } from '@/app/providers'
+import { LogOutIcon } from '@/shared'
 import { ThemeSwitcher } from '@/widgets'
 import { FC } from 'react'
+import { useNavigate } from 'react-router-dom'
 import styles from './Navbar.module.scss'
 import logo from '/public/icons/favicon-96x96.png'
 
 const Navbar: FC = () => {
+  const auth = useAuth()
+  const navigate = useNavigate()
+
+  const handleLogOut = () => {
+    auth?.signOut(() => {
+      navigate('/login')
+    })
+  }
+
   return (
     <div className={styles.container}>
-      <div className={styles.logoWrapper}>
-        <img src={logo} alt='Логотип "Mark Notes"' className={styles.logo} />
-        <h1 className={styles.title}>Mark Notes</h1>
+      <div className={styles.actions}>
+        <button type='button' onClick={handleLogOut} className={styles.logOut}>
+          <LogOutIcon />
+        </button>
+        <ThemeSwitcher />
       </div>
-      <ThemeSwitcher />
+      <div className={styles.logoWrapper}>
+        <h1 className={styles.title}>Mark Notes</h1>
+        <img src={logo} alt='Логотип "Mark Notes"' className={styles.logo} />
+      </div>
     </div>
   )
 }
