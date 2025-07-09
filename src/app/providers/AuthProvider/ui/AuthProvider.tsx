365be60a60547a5ec10a4f97a4ab9b52d710a166
Andrew Rusanov <rusanow.andrej2018@yandex.ru>
2025-07-09 13:33:35 +0300

    fix: fix auth

--- src/app/providers/AuthProvider/ui/AuthProvider.tsx
+++ src/app/providers/AuthProvider/ui/AuthProvider.tsx
@@ -8,7 +8,10 @@ interface Props {
 }
 
 export const AuthProvider: FC<Props> = ({ children }) => {
-  const [user, setUser] = useState<SignInFormData | null>(null)
+  const [user, setUser] = useState<SignInFormData | null>(() => {
+    const storedUser = localStorage.getItem('currentUser')
+    return storedUser ? JSON.parse(storedUser) : null
+  })
 
   const signUp = (newUser: SignUpFormData, callback: () => void) => {
     localStorage.setItem(newUser.email, JSON.stringify(newUser))
@@ -16,10 +19,12 @@ export const AuthProvider: FC<Props> = ({ children }) => {
   }
   const signIn = (user: SignInFormData, callback: () => void) => {
     setUser(user)
+    localStorage.setItem('currentUser', JSON.stringify(user))
     callback()
   }
   const signOut = (callback: () => void) => {
     setUser(null)
+    localStorage.removeItem('currentUser')
     callback()
   }
 
